e417b4f884bc67cdd5052da6b7d338de27fc77aa
Toros Kutlu <toros_kutlu@hotmail.com>
2022-05-25 15:43:39 +0200

    låna bok med error management

--- src/main/java/com/example/bibliotekfrontend/ControllerBorrowBookPageForCustomer.java
+++ src/main/java/com/example/bibliotekfrontend/ControllerBorrowBookPageForCustomer.java
@@ -10,15 +10,23 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ControllerBorrowBookPageForCustomer {
 
     @FXML
     private Label selectedBookForBorrow;
+
+    @FXML
+    private Label borrowErrorConfirmation;
+
     @FXML
     private TextField searchBorrowBookTextField;
     @FXML
@@ -27,6 +35,8 @@ public class ControllerBorrowBookPageForCustomer {
     private ListView<String> borrowBookListView;
     @FXML
     private Button backToCustomerFirstPageButton;
+
+    private String selectedBookString;
 
     ConnectionManager connectionManager = new ConnectionManager();
     Utility u = new Utility();
@@ -65,7 +75,9 @@ public class ControllerBorrowBookPageForCustomer {
         borrowBookListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                String selectedBookString = borrowBookListView.getSelectionModel().getSelectedItem();
+
+                //String selectedBookString = borrowBookListView.getSelectionModel().getSelectedItem();
+                selectedBookString = borrowBookListView.getSelectionModel().getSelectedItem();
                 String selectedTitle = u.getTitleFromString(selectedBookString);
                 selectedBookForBorrow.setText(selectedTitle);
 
@@ -76,6 +88,51 @@ public class ControllerBorrowBookPageForCustomer {
     @FXML
     private void cBorrowBook(ActionEvent event) {
 
-    response = connectionManager.sendGetRequest("/borrow_book/?check_book=");
+        // we must get pnr from .txt file
+        String customer_pnr_live = "";
+        try {
+            File file = new File("src/main/resources/com/example/bibliotekfrontend/customer_pnr_txt_file.txt");
+            Scanner scanner = new Scanner(file);
+            customer_pnr_live = scanner.next();
+            //while (scanner.hasNext()) {
+            //    customer_pnr_from_file = scanner.next()
+            //}
+            //customer_pnr_from_file = scanner.hasNext();
+            //customer_pnr_from_file.appendText()
+
+            // YourTextArea.appendText(s.nextInt() + " ");
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex);
+        }
+
+        customer_pnr_live = u.encodeToURL(customer_pnr_live);
+        System.out.println(customer_pnr_live);
+
+        // we must get ISBN nr from highlighted book
+        String ISBN_book_live = "";
+        ISBN_book_live = u.getIsbnFromSelectedString(selectedBookString);
+        ISBN_book_live = u.encodeToURL(ISBN_book_live);
+        System.out.println(ISBN_book_live);
+
+
+        // we need customer_pnr from logged in customer
+        // we need book_ISBN
+        // feed them to stored procedure borrow book
+
+       // check_book ??
+    // response = connectionManager.sendGetRequest("/borrow_book/?check_book=" + );
+
+        response = connectionManager.sendGetRequest("/borrow_book?customer_pnr_live=" + customer_pnr_live + "&ISBN_book_live=" + ISBN_book_live);
+        System.out.println(response);
+        if (response.contains("qty")) {
+            borrowErrorConfirmation.setVisible(true);
+            borrowErrorConfirmation.setText("Denna bok är tyvärr otillgänglig för tillfället");
+            borrowErrorConfirmation.setTextFill(Color.RED);
+        } else if (response.contains("success")) {
+            borrowErrorConfirmation.setVisible(true);
+            borrowErrorConfirmation.setText("Du har lånat denna bok!");
+            borrowErrorConfirmation.setTextFill(Color.GREEN);
+        }
+
     }
 }
